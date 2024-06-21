4a739530b7eb0d40cf7e406b447b950980753cf0
Lucas Matheus <164392998+nirmata1618@users.noreply.github.com>
2024-06-20 22:46:15 -0300

    First Version

---
+++ home.py
@@ -0,0 +1,31 @@
+def adicionar_notas(aluno, quantidade_notas=3):
+    for i in range(1, quantidade_notas + 1):
+        while True:
+            try:
+                nota = float(input(f"Insira a nota {i}: "))
+                if 0 <= nota <= 10:
+                    aluno.append(nota)
+                    break
+                else:
+                    print("Nota inválida. A nota deve estar entre 0 e 10.")
+            except ValueError:
+                print("Entrada inválida. Por favor, insira um número válido.")
+    return aluno
+
+def calcular_media_e_resultado(aluno):
+    if not aluno:
+        return 0.0, "Reprovado"
+
+    media = sum(aluno) / len(aluno)
+    if media >= 6.0:
+        resultado = "Aprovado"
+    else:
+        resultado = "Reprovado"
+
+    return media, resultado
+
+notas_aluno = []
+notas_aluno = adicionar_notas(notas_aluno)
+media, resultado = calcular_media_e_resultado(notas_aluno)
+print(f"Média: {media:.2f}")
+print(f"Resultado: {resultado}")
